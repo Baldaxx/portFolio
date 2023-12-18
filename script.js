d8ce8be01c793c3e08c346527dca54953744cebc
virginie.bm13@icloud.com <virginie.bm13@icloud.com>
2023-12-18 15:04:03 +0100

    compte à rebours

--- script.js
+++ script.js
@@ -135,14 +135,6 @@ let ebody = `
 	);
 })
 
-/*
-document.querySelector('.butt').addEventListener('click', function(event) {
-    event.preventDefault(); // Empêche le comportement par défaut du lien
-    startGame(); // Appel à la fonction startGame() pour réinitialiser le jeu
-    return false;
-});
-*/
-
 function startStopClicked() {
 	if(document.getElementById("start_stop_btn").innerHTML == "STOP") {
 		stopGame()
@@ -182,3 +174,71 @@ var verification = setInterval(function() {
         alert("Vous êtes mort...!")
     }
 }, 1);
+
+const countdownEls = document.querySelectorAll(".countdown")
+countdownEls.forEach(countdownEl => createCountdown(countdownEl))
+
+function createCountdown(countdownEl){
+  const target = new Date(new Date(countdownEl.dataset.targetDate).toLocaleString('en', ))
+  const parts = {
+    days: {text: ["days","day"], dots: 30},
+    hours: {text: ["hours","hour"], dots: 24},
+    minutes: {text: ["minutes","minute"], dots: 60},
+    seconds: {text: ["seconds","second"], dots: 60},
+  }
+
+  Object.entries(parts).forEach(([key, value])=>{
+    const partEl = document.createElement("div");
+    partEl.classList.add("part", key);
+    partEl.style.setProperty("--dots", value.dots);
+    value.element = partEl;
+
+    const remainingEl = document.createElement("div");
+    remainingEl.classList.add("remaining");
+    remainingEl.innerHTML = `
+      <span class="number"></span>
+      <span class="text"></span>
+    `
+    partEl.append(remainingEl);
+    for(let i = 0; i < value.dots; i++){
+      const dotContainerEl = document.createElement("div");
+      dotContainerEl.style.setProperty("--dot-idx", i);
+      dotContainerEl.classList.add("dot-container")
+      const dotEl = document.createElement("div");
+      dotEl.classList.add("dot")
+      dotContainerEl.append(dotEl);
+      partEl.append(dotContainerEl);
+    }
+    countdownEl.append(partEl);
+  })
+  getRemainingTime(target, parts)
+}
+
+function getRemainingTime(target, parts, first=true){
+  const now = new Date()
+  if(first) console.log({target, now})
+  const remaining = {}
+  let seconds = Math.floor((target - (now))/1000);
+  let minutes = Math.floor(seconds/60);
+  let hours = Math.floor(minutes/60);
+  let days = Math.floor(hours/24);
+  hours = hours-(days*24);
+  minutes = minutes-(days*24*60)-(hours*60);
+  seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+  Object.entries({days, hours, minutes, seconds}).forEach(([key, value])=>{
+    const remaining = parts[key].element.querySelector(".number");
+    const text = parts[key].element.querySelector(".text");
+    remaining.innerText = value;
+    text.innerText = parts[key].text[Number(value==1)]
+    const dots = parts[key].element.querySelectorAll(".dot")
+    dots.forEach((dot, idx)=>{
+      dot.dataset.active = idx <= value;
+      dot.dataset.lastactive = idx == value;
+    })
+  })
+  if(now <= target){
+    window.requestAnimationFrame(()=>{
+      getRemainingTime(target, parts, false)
+    });
+  }
+}
